c090b8d20b807d7c3864d67d5068e508008e5836
astrocoding <reepalstudio01@gmail.com>
2024-06-05 04:13:17 +0700

    add app.js and package

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+db.connect(err => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+  } else {
+    console.log('Connected to MySQL');
+  }
+});
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+const authRoutes = require('./routes/authRoutes');
+const landRoutes = require('./routes/landRoutes');
+const rentalRoutes = require('./routes/rentalRoutes');
+const transactionRoutes = require('./routes/transactionRoutes');
+
+app.use('/auth', authRoutes);
+app.use('/lands', landRoutes);
+app.use('/rentals', rentalRoutes);
+app.use('/transactions', transactionRoutes);
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
