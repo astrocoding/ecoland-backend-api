dc282610f5dca5437584a9850e0e1ad616f05cce
astrocoding <reepalstudio01@gmail.com>
2024-06-19 01:40:34 +0700

    Refactor middleware/AuthUser.js

--- routes/rentalRoutes.js
+++ routes/rentalRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const rentalController = require('../controllers/rentalController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/AuthUser');
 
 router.post('/', authMiddleware, rentalController.createRental);
 router.get('/', authMiddleware, rentalController.getRentalsByUserId);
