bfac33784f5ed7dc5ddad438ffd654da5f759869
Zaenal Alfian <118641939+astrocoding@users.noreply.github.com>
2024-06-19 23:21:10 +0700

    Update AuthUser.js

--- middleware/AuthUser.js
+++ middleware/AuthUser.js
@@ -1,27 +1,42 @@
+import jwt from "jsonwebtoken";
 import User from "../models/UserModel.js";
 
-export const verifyUser = async (req, res, next) =>{
-    if(!req.session.userId){
-        return res.status(401).json({msg: "Mohon login ke akun Anda!"});
-    }
-    const user = await User.findOne({
-        where: {
-            uuid: req.session.userId
-        }
-    });
-    if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
-    req.userId = user.id;
-    req.role = user.role; 
-    next();
-}
+// Secret key untuk menandatangani token JWT
+const SECRET_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3MTg4MTM5MDIsImV4cCI6MTc1MDM0OTkwMiwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.2HsoyOpk4pUbm71yBHpBVDgvRhxUSgMkmbN-eW6scGQ';
 
-export const adminOnly = async (req, res, next) =>{
-    const user = await User.findOne({
-        where: {
-            uuid: req.session.userId
-        }
+export const verifyUser = async (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({ msg: "Mohon login ke akun Anda!" });
+
+    jwt.verify(token, SECRET_KEY, async (err, decoded) => {
+        if (err) return res.status(403).json({ msg: "Token tidak valid" });
+        const user = await User.findOne({
+            where: {
+                uuid: decoded.uuid
+            }
+        });
+        if (!user) return res.status(404).json({ msg: "User tidak ditemukan" });
+        req.userId = user.id;
+        req.role = user.role;
+        next();
     });
-    if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
-    if(user.role !== "admin") return res.status(403).json({msg: "Akses terlarang"});
-    next();
-}
+};
+
+export const adminOnly = async (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) return res.status(401).json({ msg: "Mohon login ke akun Anda!" });
+
+    jwt.verify(token, SECRET_KEY, async (err, decoded) => {
+        if (err) return res.status(403).json({ msg: "Token tidak valid" });
+        const user = await User.findOne({
+            where: {
+                uuid: decoded.uuid
+            }
+        });
+        if (!user) return res.status(404).json({ msg: "User tidak ditemukan" });
+        if (user.role !== "admin") return res.status(403).json({ msg: "Akses terlarang" });
+        next();
+    });
+};
