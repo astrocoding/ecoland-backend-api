74e9abe96f31bacd07bf6e8266925142ded99f52
Zaenal Alfian <118641939+astrocoding@users.noreply.github.com>
2024-06-20 00:20:45 +0700

    Update AuthRoute.js

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,14 +1,11 @@
 import express from "express";
-import { Login, Register, logOut, Me } from "../controllers/Auth.js";
-import { verifyUser, adminOnly } from "../middleware/AuthUser.js";
+import {Login, Register, logOut, Me} from "../controllers/Auth.js";
 
 const router = express.Router();
 
-router.get('/me', verifyUser, Me);
+router.get('/me', Me);
 router.post('/login', Login);
 router.post('/register', Register);
-
-// Untuk route logout, tidak perlu middleware karena JWT bersifat stateless dan tidak memerlukan logout server-side
-router.delete('/logout', logOut); 
+router.delete('/logout', logOut);
 
 export default router;
