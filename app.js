ec3d3814d8a9d950e18e43263824ba4b059f5734
Zaenal Alfian <118641939+astrocoding@users.noreply.github.com>
2024-06-20 00:24:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
 import express from "express";
 import cors from "cors";
+import session from "express-session";
 import dotenv from "dotenv";
 import { Storage } from '@google-cloud/storage';
 import multer from 'multer';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import db from "./config/db.js";
+import SequelizeStore from "connect-session-sequelize";
 import UserRoute from "./routes/UserRoute.js";
 import LandRoute from "./routes/LandRoute.js";
 import AuthRoute from "./routes/AuthRoute.js";
 import TransactionRoute from "./routes/TransactionsRoute.js";
 import HistoryRoute from "./routes/HistoryRoute.js";
-import bodyParser from 'body-parser';
-import jwt from "jsonwebtoken";
-
 dotenv.config();
 
 const app = express();
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
 
 // Konfigurasi ES Module untuk mendapatkan __dirname
 const __filename = fileURLToPath(import.meta.url);
@@ -29,42 +24,39 @@ const __dirname = path.dirname(__filename);
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Sinkronisasi database
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({
+    db: db
+});
+
 (async()=>{
     await db.sync();
 })();
 
-// Middleware untuk memverifikasi token JWT
-const verifyToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (!token) return res.status(401).json({ msg: "Mohon login ke akun Anda!" });
-
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-        if (err) return res.status(403).json({ msg: "Token tidak valid" });
-        req.userId = decoded.uuid;
-        next();
-    });
-};
-
-// app.use(cors({
-//     credentials: true,
-//     origin: ['https://ecoland-frontend.vercel.app/', 'http://localhost:5173']
-// }));
+app.use(session({
+    secret: 'hxaqfgKv172cvNFvzKmLFkVhYPYWNXQc',
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+        secure: 'auto'
+    }
+}));
 
 app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    credentials: true,
+    origin: 'https://ecoland-frontend.vercel.app'
 }));
 
 app.use(express.json());
-app.use(AuthRoute);
 app.use(UserRoute);
 app.use(LandRoute);
+app.use(AuthRoute);
 app.use(TransactionRoute);
 app.use(HistoryRoute);
+
+store.sync();
 
 // Google Cloud Storage setup
 const gc = new Storage({
@@ -108,13 +100,9 @@ app.post('/upload', upload.single('image'), (req, res) => {
     blobStream.end(req.file.buffer);
 });
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Server berhasil berjalan tanpa masalah' });
-});
-
 const PORT = 5000;
-const HOST = '0.0.0.0';
 
-app.listen(PORT, HOST, ()=> {
+
+app.listen(PORT, ()=> {
     console.log('Server up and running...');
 });
